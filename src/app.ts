e63a69b6c0489d37cdf01a295774de0053c491fc
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-08-27 20:47:50 -0300

    created routes archive

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 
+import { routes } from './routes';
+
 import './database/connect';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(routes);
 
 export { app };
